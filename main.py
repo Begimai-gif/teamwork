6a96c11abe4e00678545cd19bf0a159a48c815d7
svoy <seniorkranzit@gmail.com>
2026-07-02 20:57:45 +0600

    added new feature from new branch

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+
 print('hello world')
 #text
 mmmm='jfjfjfjfjfjf'
+
+# print('hello world')
+
+sales = [1200, 850, 430, 2100, 1750]
+
+
+def calculate_total(sales):
+    
+    return sum(sales)
+
+
+def calculate_average(sales):
+    return calculate_total(sales)/len(sales)
+
+    
+
+print(calculate_total(sales))
+print(calculate_average(sales))
+
+
+
+
+
+new_feature=1333232
+def sub(a,b):
+    return a-b
+print(sub(23,22))
+print(sub(new_feature,222))
+
