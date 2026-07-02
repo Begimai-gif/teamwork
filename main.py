21b85a0702c887400d002eb8e74ad4a7f65585a4
begimau <sansyzbaevabegimai@gmail.com>
2026-07-02 20:33:31 +0600

    added calculate

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # print('hello world')
 
 sales = [1200, 850, 430, 2100, 1750]
+
+
+def calculate_total(sales):
+    
+    return sum(sales)
+
+
+def calculate_average(sales):
+    return calculate_total(sales)/len(sales)
+
+    
+
+print(calculate_total(sales))
+print(calculate_average(sales))
